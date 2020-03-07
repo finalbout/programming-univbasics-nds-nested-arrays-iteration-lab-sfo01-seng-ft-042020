e8f7ff7be7a06e8d012060ac4cbc2fb0cb1e8729
John Tom Jr <johntjr.22@gmail.com>
2020-03-07 07:04:21 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -4,7 +4,7 @@ def join_ingredients(src)
   i = 0 
   while i < src.length do 
     inner = src[i]
-    result.push "I love [inner] "
+    result.push "I love #[inner[0]] and  "
   
   # Given an Array of 2-element Arrays ( [ [food1, food2], [food3, # food4]....[foodN, foodM]]):
   #
